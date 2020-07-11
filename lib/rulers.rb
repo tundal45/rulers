2a41875a3f416fd47b254bf3ac81892ed9ce3b7f
Ashish Dixit <tundal45@gmail.com>
2020-07-11 13:37:36 -0700

    Adds Array#sum method to rulers and uses it

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,9 +1,10 @@
 require "rulers/version"
+require "rulers/array"
 
 module Rulers
   class Application
     def call(env)
-      [200, {"Content-Type" => "text/html"}, ["Hello from Ruby on Rulers!"]]
+      [200, {"Content-Type" => "text/html"}, ["Hello from Ruby on Rulers! #{[1, 2, 37, 9].sum}"]]
     end
   end
 end
