7936b9f0f446ea3e0afccc32f03c952b387cd3a5
Ashish Dixit <tundal45@gmail.com>
2020-07-12 10:42:46 -0700

    Defaults to HomeController in routing logic

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,14 +3,9 @@ require "rulers/routing"
 
 module Rulers
   class Application
-    attr_accessor :root
-
     def call(env)
       if env["PATH_INFO"] == "/favicon.ico"
         return [404, {"Content-Type" => "text/html"}, []]
-      elsif env["PATH_INFO"] == "/"
-        default_page = File.read("#{root}/public/index.html")
-        return [200, {"Content-Type" => "text/html"}, [default_page]]
       end
 
       klass, act = get_controller_and_action(env)
--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -1,7 +1,13 @@
 module Rulers
   class Application
     def get_controller_and_action(env)
-      _, cont, action, after = env["PATH_INFO"].split("/", 4)
+      if env["PATH_INFO"] == "/"
+        cont = "home"
+        action = "index"
+      else
+        _, cont, action, after = env["PATH_INFO"].split("/", 4)
+      end
+
       cont = "#{cont.capitalize}Controller"
 
       [Object.const_get(cont), action]
